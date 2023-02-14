6bb64953b5b62dbe2778304f6288baf9de69b277
zewdu erkyhun <zewdu444@gmail.com>
2023-02-14 23:35:10 +0300

    Refactor to functional component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,57 +1,49 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 import './Calculator.css';
 
-export default class Calculator extends Component {
-  constructor() {
-    super();
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-  }
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
+  const calculatorHandler = (e) => {
+    setState((state) => (calculate(state, e.target.textContent)));
+  };
+  const { total, operation, next } = state;
+  return (
+    <div className="container">
+      <section className="calulator">
+        <div className="number-input">
+          {total}
+          {operation}
+          {next}
+        </div>
+        <div className="calc-button-holder">
+          <button type="button" className="calc-button" onClick={calculatorHandler}>AC</button>
+          <button type="button" className="calc-button" onClick={calculatorHandler}>+/-</button>
+          <button type="button" className="calc-button" onClick={calculatorHandler}>%</button>
+          <button type="button" className="calc-button orange" onClick={calculatorHandler}>&divide;</button>
+          <button type="button" className="calc-button" onClick={calculatorHandler}>7</button>
+          <button type="button" className="calc-button" onClick={calculatorHandler}>8</button>
+          <button type="button" className="calc-button" onClick={calculatorHandler}>9</button>
+          <button type="button" className="calc-button orange" onClick={calculatorHandler}>x</button>
+          <button type="button" className="calc-button" onClick={calculatorHandler}>4</button>
+          <button type="button" className="calc-button" onClick={calculatorHandler}>5</button>
+          <button type="button" className="calc-button" onClick={calculatorHandler}>6</button>
+          <button type="button" className="calc-button orange" onClick={calculatorHandler}>-</button>
+          <button type="button" className="calc-button" onClick={calculatorHandler}>1</button>
+          <button type="button" className="calc-button" onClick={calculatorHandler}>2</button>
+          <button type="button" className="calc-button" onClick={calculatorHandler}>3</button>
+          <button type="button" className="calc-button orange" onClick={calculatorHandler}>+</button>
+          <button type="button" className="calc-button span-2" onClick={calculatorHandler}>0</button>
+          <button type="button" className="calc-button" onClick={calculatorHandler}>.</button>
+          <button type="button" className="calc-button orange" onClick={calculatorHandler}>=</button>
+        </div>
+      </section>
+    </div>
+  );
+};
 
-  calculatorHandler =(e) => {
-    this.setState((state) => (calculate(state, e.target.textContent)));
-    return this;
-  }
-
-  render() {
-    const {
-      total, next, operation,
-    } = this.state;
-    return (
-      <div className="container">
-        <section className="calulator">
-          <div className="number-input">
-            {total}
-            {operation}
-            {next}
-          </div>
-          <div className="calc-button-holder">
-            <button type="button" className="calc-button" onClick={this.calculatorHandler}>AC</button>
-            <button type="button" className="calc-button" onClick={this.calculatorHandler}>+/-</button>
-            <button type="button" className="calc-button" onClick={this.calculatorHandler}>%</button>
-            <button type="button" className="calc-button orange" onClick={this.calculatorHandler}>&divide;</button>
-            <button type="button" className="calc-button" onClick={this.calculatorHandler}>7</button>
-            <button type="button" className="calc-button" onClick={this.calculatorHandler}>8</button>
-            <button type="button" className="calc-button" onClick={this.calculatorHandler}>9</button>
-            <button type="button" className="calc-button orange" onClick={this.calculatorHandler}>x</button>
-            <button type="button" className="calc-button" onClick={this.calculatorHandler}>4</button>
-            <button type="button" className="calc-button" onClick={this.calculatorHandler}>5</button>
-            <button type="button" className="calc-button" onClick={this.calculatorHandler}>6</button>
-            <button type="button" className="calc-button orange" onClick={this.calculatorHandler}>-</button>
-            <button type="button" className="calc-button" onClick={this.calculatorHandler}>1</button>
-            <button type="button" className="calc-button" onClick={this.calculatorHandler}>2</button>
-            <button type="button" className="calc-button" onClick={this.calculatorHandler}>3</button>
-            <button type="button" className="calc-button orange" onClick={this.calculatorHandler}>+</button>
-            <button type="button" className="calc-button span-2" onClick={this.calculatorHandler}>0</button>
-            <button type="button" className="calc-button" onClick={this.calculatorHandler}>.</button>
-            <button type="button" className="calc-button orange" onClick={this.calculatorHandler}>=</button>
-          </div>
-        </section>
-      </div>
-    );
-  }
-}
+export default Calculator;
