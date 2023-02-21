7dd075453260b146e44c240948532c9dcdb1bbff
zewdu erkyhun <zewdu444@gmail.com>
2023-02-21 18:39:10 +0300

    Modify reame file

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './NavBar.css';
 
 function NavBar() {
   return (
     <div>
       <nav>
-        <div className="nav-wrapper teal">
-          <a href="/" className="brand-logo left logospace">Math Magicians</a>
+        <div className="nav-wrapper orange">
+          <a href="/" className="brand-logo left logo_postion hide-on-small-only">Math Magicians</a>
+          <a href="/" className="brand-logo left logo_postion hide-on-med-and-up"><i className="large material-icons ">home</i></a>
           <ul id="nav-mobile" className="right">
             <li><NavLink to="/">Home</NavLink></li>
             <li><NavLink to="/Calculator">calculator</NavLink></li>
--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import './Quotes.css';
 
 function Quotes() {
   return (
-    <div>Quotes</div>
+
+    <div className="row left card_position">
+      <div className="col s12">
+        <div className="card ">
+          <div className="card-content">
+            <h5>
+              Mathematics is not about numbers, equations, computations
+              or algorithms; it is about understanding.
+              <b>-William Paul Thurston-</b>
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
